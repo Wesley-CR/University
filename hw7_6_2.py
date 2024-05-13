4ed2073493e3cdfe57b8c4add6b340345f3a5cb2
Wesley-CR <152462437+Wesley-CR@users.noreply.github.com>
2024-05-12 23:53:33 -0600

    hw7 advances
    little advance but I want to keep the streak lol

--- hw7_6_2.py
+++ hw7_6_2.py
@@ -1,4 +1,5 @@
-testTree = [2,[[3,[]],[4,[]],[5,[[10,[]]]]]]
+testTree = [6,[[3,[]],[4,[[8,[]],[9,[]]]],[2,[[10,[]]]]]]
+#1
 # Height
 def tree_height(T):
     if T == []: return 0
@@ -9,6 +10,7 @@ def tree_height(T):
     return mx + 1
 
 print(tree_height(testTree))
+#2. Max and Min
 # Most value node
 def most_value_node(T):
     if T == []: return -1
@@ -38,7 +40,7 @@ print(most_value_node_path(testTree))
 
 # Least value node
 def least_value_node(T):
-    if T == []: return 100000
+    if T == []: return -1
     v, hjs = T
     minNode = v
     for h in hjs:
@@ -47,7 +49,7 @@ def least_value_node(T):
             minNode = result
     return minNode
 
-print(most_value_node(testTree))
+print(least_value_node(testTree))
 # Path to least value node
 def least_value_node_path(T):
     if T == []: return T
@@ -62,3 +64,45 @@ def least_value_node_path(T):
     return path
 
 print(least_value_node_path(testTree))
+#3 Order
+print("============================================")
+# Pre-order
+def pre_order(T):
+    if T == []: return
+    v, chldrn = T
+    print(v)
+    for chld in chldrn:
+        pre_order(chld)
+
+pre_order(testTree)
+print("============================================")
+# From now on im going to assume the trees are binary trees :D
+bnTree = [5, [6, [3, [], []], []], [3, [], [7, [9, [], []], []]]]
+#In order
+def in_order(T):
+    if T == []: return
+    if len(T) == 3:
+        v, L, R = T
+        in_order(L)
+        print(v)
+        in_order(R)
+    else:
+        print(T[0])
+
+in_order(bnTree)
+
+print("============================================")
+#Post order
+def post_order(T):
+    if T == []: return
+    if len(T) == 3:
+        v, L, R = T
+        post_order(L)
+        post_order(R)
+        print(v)
+    else:
+        print(T[0])
+
+post_order(bnTree)
+
+print("============================================")
